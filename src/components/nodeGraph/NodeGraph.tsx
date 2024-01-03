a671e4b16dedde1cbf186946985d6cd729725085
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-01-04 00:08:52 +0200

    add node graph

--- src/components/nodeGraph/NodeGraph.tsx
+++ src/components/nodeGraph/NodeGraph.tsx
@@ -1,75 +1,33 @@
-// src/components/NodeGraph.tsx
-import React, { useCallback, useState } from 'react'
-import { useStoreSelector, useAppDispatch } from '../../redux/store/index'
-import ReactFlow, {
-  addEdge,
-  FitViewOptions,
-  applyNodeChanges,
-  applyEdgeChanges,
-  DefaultEdgeOptions,
-  Node,
-  Edge,
-  OnNodesChange,
-  OnEdgesChange,
-  OnConnect,
-  NodeTypes,
-} from 'reactflow'
-import 'reactflow/dist/style.css'
+import React from 'react'
+import { Handle, NodeProps, Position } from 'reactflow'
 import NodeSelect from '../nodeSelect/NodeSelect'
+import styles from './NodeGraph.module.scss'
 
-const initialNodes: Node[] = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-]
-const initialEdges: Edge[] = [{ id: 'e1-2', source: '1', target: '2' }]
-
-const defaultEdgeOptions: DefaultEdgeOptions = {
-  animated: true,
-}
-
-// const nodeTypes: NodeTypes = {
-//   custom: CustomNode,
-// }
-
-const fitViewOptions: FitViewOptions = {
-  padding: 0.2,
-}
-
-const NodeGraph: React.FC = ({ id, data, isConnectable }) => {
-  const [nodes, setNodes] = useState<Node[]>(initialNodes)
-  const [edges, setEdges] = useState<Edge[]>(initialEdges)
-
-  const onNodesChange: OnNodesChange = useCallback(
-    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
-    [setNodes],
-  )
-  const onEdgesChange: OnEdgesChange = useCallback(
-    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-    [setEdges],
-  )
-  const onConnect: OnConnect = useCallback(
-    (connection) => setEdges((eds) => addEdge(connection, eds)),
-    [setEdges],
-  )
-
+const NodeGraph: React.FC<NodeProps> = ({ id, data, isConnectable }) => {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        fitView
-        fitViewOptions={fitViewOptions}
-        defaultEdgeOptions={defaultEdgeOptions}
-        // nodeTypes={nodeTypes}
-      />
-      <NodeSelect
-        options={data.options}
-        selectedValues={data.selectedValues}
-        placeholder={data.placeholder}
-        id={id}
+    <div className={`customNode ${data.selected ? 'selected' : ''}`} id={id}>
+      {id !== '0' && (
+        <Handle
+          type="target"
+          position={Position.Top}
+          isConnectable={isConnectable}
+        />
+      )}
+      <div className="node-header">{data.label}</div>
+      <div className={styles.nodeContent}>
+        <textarea className={styles.nodeTextarea} />
+        <NodeSelect
+          options={data.options}
+          selectedValues={data.selectedValues}
+          placeholder={data.placeholder}
+          id={id}
+        />
+      </div>
+      <Handle
+        type="source"
+        position={id === '0' ? Position.Right : Position.Bottom}
+        style={id === '0' ? { top: 116 } : { top: 'none' }}
+        isConnectable={isConnectable}
       />
     </div>
   )
--- src/types/interface.ts
+++ src/types/interface.ts
@@ -1,3 +1,8 @@
+export interface IOption {
+  value: string
+  label: string
+}
+
 export interface SelectOption {
   value: string
   label: string
