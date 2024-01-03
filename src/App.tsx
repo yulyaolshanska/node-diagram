7cc62515ce0d0fee018ade2d31fdc9c051c59cda
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-01-04 01:00:13 +0200

    feat: add onNodesChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import React, { useMemo } from 'react'
 import { useSelector } from 'react-redux'
-import ReactFlow from 'reactflow'
-import { RootState } from './redux/store'
+import ReactFlow, { OnNodesChange } from 'reactflow'
+import { RootState, useAppDispatch } from './redux/store'
 import NodeGraph from './components/nodeGraph/NodeGraph'
 import 'reactflow/dist/style.css'
+import { nodeChanged } from './redux/slices/nodeSlice'
 
 const App: React.FC = () => {
+  const dispatch = useAppDispatch()
   const { nodes, edges } = useSelector((state: RootState) => state.nodes)
 
   const nodeTypes = useMemo(() => ({ customNode: NodeGraph }), [])
 
+  const onNodesChange: OnNodesChange = React.useCallback(
+    (changes) => dispatch(nodeChanged(changes)),
+    [dispatch],
+  )
+
   return (
     <div className="app" style={{ height: '100vh', width: '100vw' }}>
-      <ReactFlow nodeTypes={nodeTypes} nodes={nodes} edges={edges} />
+      <ReactFlow
+        nodeTypes={nodeTypes}
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+      />
     </div>
   )
 }
